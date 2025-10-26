ac142d302cfe8f7ae5d5a0bb81367d9c695cadff
Franko <102835462+Franklin-O@users.noreply.github.com>
2025-10-25 23:10:56 -0600

    Add files via upload

---
+++ UDPserver.py
@@ -0,0 +1,12 @@
+from socket import *
+
+server_port = 12000
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind(('', server_port))
+print("UDP server is ready to receive...")
+
+while True:
+    message, client_address = server_socket.recvfrom(2048)
+    modified_message = message.decode().upper()
+    print(f"Received from {client_address}: {message.decode()}")
+    server_socket.sendto(modified_message.encode(), client_address)
